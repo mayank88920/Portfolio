8ac72c2b3490a7e4e5952aea4abaa66ac343cd2a
mayank88920 <mraj88920@gmail.com>
2024-03-13 23:13:06 +0530

    new commit

--- Mayank-Portfolio/src/app/app.routes.ts
+++ Mayank-Portfolio/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { IntroComponent } from './Pages/intro/intro.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { ProjectsComponent } from './Pages/projects/projects.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+        pathMatch: 'full',
+      },
+      {
+        path:'home',
+        component: HomeComponent,
+        pathMatch: 'full',
+      },
+      {
+        path:'intro',
+        component: IntroComponent,
+        pathMatch: 'full',
+      },
+      {
+        path:'contact',
+        component: ContactComponent,
+        pathMatch: 'full',
+      },
+      {
+        path:'projects',
+        component: ProjectsComponent,
+        pathMatch: 'full',
+      },
+];
